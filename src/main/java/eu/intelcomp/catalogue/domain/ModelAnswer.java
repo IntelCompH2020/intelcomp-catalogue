66b04e8592271feffbc48aee9ee97195a48368e9
Konstantinos Spyrou <spyroukon@gmail.com>
2022-02-28 18:10:02 +0200

    added wrapper class for compatibility with frontend lib

---
+++ src/main/java/eu/intelcomp/catalogue/domain/ModelAnswer.java
@@ -0,0 +1,39 @@
+package eu.intelcomp.catalogue.domain;
+
+import org.json.simple.JSONObject;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ModelAnswer {
+
+    private String id;
+    private Map<String, ChapterAnswer> chapterAnswers;
+
+    public ModelAnswer() {
+        this.chapterAnswers = new TreeMap<>();
+    }
+
+    public ModelAnswer(JSONObject answer) {
+        this.chapterAnswers = new TreeMap<>();
+        String answerId = answer.get("id").toString();
+        this.id = answerId;
+        this.chapterAnswers.put(answerId, new ChapterAnswer(answerId, answer));
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Map<String, ChapterAnswer> getChapterAnswers() {
+        return chapterAnswers;
+    }
+
+    public void setChapterAnswers(Map<String, ChapterAnswer> chapterAnswers) {
+        this.chapterAnswers = chapterAnswers;
+    }
+}
