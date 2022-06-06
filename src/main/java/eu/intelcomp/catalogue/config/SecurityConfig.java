601a7153e8b29cb2634aad696be15c293cd9500f
Konstantinos Spyrou <spyroukon@gmail.com>
2022-06-06 17:05:29 +0300

    added authorization for some lib methods

--- src/main/java/eu/intelcomp/catalogue/config/SecurityConfig.java
+++ src/main/java/eu/intelcomp/catalogue/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authorizeRequests -> authorizeRequests
-//                        .regexMatchers("/forms/.*", "/dump/.*", "/restore/", "/resources.*", "/resourceType.*", "/search.*").hasAnyAuthority("ADMIN")
+                        .regexMatchers("/forms/.*", "/dump/.*", "/restore/", "/resources.*", "/resourceType.*", "/search.*", "/logs.*").hasAnyAuthority("ADMIN")
                         .anyRequest().permitAll())
                 .oauth2Login()
                 .successHandler(authSuccessHandler)
