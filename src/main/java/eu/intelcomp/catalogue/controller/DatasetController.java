a10f7a24edf87a1bbeccdbe6b27a4c54a62ac561
Konstantinos Spyrou <spyroukon@gmail.com>
2022-06-29 15:45:50 +0300

    fixed null pointer exception

--- src/main/java/eu/intelcomp/catalogue/controller/DatasetController.java
+++ src/main/java/eu/intelcomp/catalogue/controller/DatasetController.java
@@ -3,6 +3,7 @@ package eu.intelcomp.catalogue.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.intelcomp.catalogue.domain.ModelAnswer;
+import eu.openminted.registry.core.domain.Resource;
 import eu.openminted.registry.core.service.SearchService;
 import gr.athenarc.catalogue.service.GenericItemService;
 import org.json.simple.JSONObject;
@@ -10,6 +11,8 @@ import org.json.simple.JSONValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,9 +48,13 @@ public class DatasetController {
 
     // TODO: helper method, remove when job filter is implemented correctly from @CITE
     @GetMapping("instances/{type}/{version}/internalid")
-    public String getCoreId(@PathVariable("type") String type, @PathVariable("version") String version) {
+    public ResponseEntity<String> getCoreId(@PathVariable("type") String type, @PathVariable("version") String version) {
         SearchService.KeyValue typeKeyValue = new SearchService.KeyValue("type", type);
         SearchService.KeyValue versionKeyValue = new SearchService.KeyValue("version", version);
-        return genericItemService.searchResource("dataset_instance", typeKeyValue, versionKeyValue).getId();
+        Resource resource = genericItemService.searchResource("dataset_instance", typeKeyValue, versionKeyValue);
+        if (resource == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(resource.getId(), HttpStatus.OK);
     }
 }
