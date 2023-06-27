c0ea582479b31f8a0908ab0e04f87c730efee270
Konstantinos Spyrou <spyroukon@gmail.com>
2023-06-27 21:12:02 +0300

    created Dataset pojo

---
+++ src/main/java/eu/intelcomp/catalogue/domain/Dataset.java
@@ -0,0 +1,93 @@
+package eu.intelcomp.catalogue.domain;
+
+import java.util.Date;
+import java.util.List;
+
+
+public class Dataset {
+
+    private String id;
+    private String name;
+    private String description;
+    private List<String> usages;
+    private String owner;
+    private Date creationDate;
+    private String createdBy;
+    private Date modificationDate;
+    private String modifiedBy;
+
+    public Dataset() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getUsages() {
+        return usages;
+    }
+
+    public void setUsages(List<String> usages) {
+        this.usages = usages;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+}
