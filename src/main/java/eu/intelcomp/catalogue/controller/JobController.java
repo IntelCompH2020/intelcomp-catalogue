0095e1ec3d0938ab97008da6857358eac89c24a9
Konstantinos Spyrou <spyroukon@gmail.com>
2023-07-06 12:56:31 +0300

    added download output file functionality

--- src/main/java/eu/intelcomp/catalogue/controller/JobController.java
+++ src/main/java/eu/intelcomp/catalogue/controller/JobController.java
@@ -8,19 +8,26 @@ import eu.intelcomp.catalogue.domain.JobInfo;
 import eu.intelcomp.catalogue.domain.User;
 import eu.intelcomp.catalogue.service.JobService;
 import gr.athenarc.catalogue.exception.ResourceException;
+import gr.athenarc.catalogue.exception.ResourceNotFoundException;
 import io.swagger.v3.oas.annotations.Parameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
@@ -29,6 +36,7 @@ import java.util.UUID;
 @RequestMapping("jobs")
 public class JobController {
 
+    private static final Logger logger = LoggerFactory.getLogger(JobController.class);
     private static final ObjectMapper mapper = new ObjectMapper();
     private final JobService jobService;
 
@@ -59,6 +67,33 @@ public class JobController {
         JobInfo info = jobService.add(job, authentication);
 //        Files.createDirectories(Paths.get(String.format("/workdir/%s/%s", job.getServiceArguments().getProcessId(), info.getCreatedAt())));
         return new ResponseEntity<>(info, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/{jobId}/output/download", method = RequestMethod.GET)
+    public ResponseEntity<Resource> download(@PathVariable("jobId") String id,
+                                             @RequestParam String process,
+                                             @RequestParam String filename,
+                                             @Parameter(hidden = true) Authentication authentication) {
+        JobInfo info = get(id, null, authentication).getBody();
+        ResponseEntity<Resource> responseEntity = ResponseEntity.notFound().build();
+        if (info != null) {
+            try {
+                File file = new File(String.format("/workdir/%s/%s/output/%s", process, id, filename));
+                Path path = Paths.get(file.getAbsolutePath());
+
+                ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+                HttpHeaders headers = new HttpHeaders();
+                headers.add(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=%s_output.cvs", id));
+                responseEntity = ResponseEntity.ok()
+                        .headers(headers)
+                        .contentLength(file.length())
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .body(resource);
+            } catch (Exception e) {
+                logger.warn("Could not find file : {}", e.getMessage());
+            }
+        }
+        return responseEntity;
     }
 
     @GetMapping("/{jobId}")
