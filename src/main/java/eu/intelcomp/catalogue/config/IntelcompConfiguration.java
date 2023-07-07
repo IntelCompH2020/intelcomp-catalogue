eb05968e0a9b1afad0589b10b4ca8f722f5f9169
Konstantinos Spyrou <spyroukon@gmail.com>
2023-07-07 15:49:47 +0300

    excluded registry GenericController advice

--- src/main/java/eu/intelcomp/catalogue/config/IntelcompConfiguration.java
+++ src/main/java/eu/intelcomp/catalogue/config/IntelcompConfiguration.java
@@ -3,9 +3,10 @@ package eu.intelcomp.catalogue.config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @EnableConfigurationProperties(IntelcompProperties.class)
-@ComponentScan(value = {"eu.openminted.registry.core"})
+@ComponentScan(value = {"eu.openminted.registry.core"}, excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = eu.openminted.registry.core.controllers.GenericController.class)})
 public class IntelcompConfiguration {
 }
